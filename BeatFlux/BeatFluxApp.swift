0fc826dd359aa7a0cd954a4929ccfbc4233cc3b6
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-04-23 14:56:01 -0400

    Initial Commit

---
+++ BeatFlux/BeatFluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatFluxApp.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BeatFluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
