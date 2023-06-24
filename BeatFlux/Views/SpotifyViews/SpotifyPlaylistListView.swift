f2151819ed0bc3079662903796d1bbd39248c6b3
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-06-23 21:43:29 -0400

    cleaned up requests

---
+++ BeatFlux/Views/SpotifyViews/SpotifyPlaylistListView.swift
@@ -0,0 +1,20 @@
+//
+//  PlaylistListView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 6/21/23.
+//
+
+import SwiftUI
+
+struct SpotifyPlaylistListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlaylistListView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotifyPlaylistListView()
+    }
+}
