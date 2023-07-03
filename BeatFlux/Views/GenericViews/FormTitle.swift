4758cc290bfdeef71b164af112252800af0d7701
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-07-03 12:16:23 -0400

    fixed performance issues

---
+++ BeatFlux/Views/GenericViews/FormTitle.swift
@@ -0,0 +1,18 @@
+//
+//  FormTitle.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 6/29/23.
+//
+
+import SwiftUI
+
+struct FormTitle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormTitle()
+}
