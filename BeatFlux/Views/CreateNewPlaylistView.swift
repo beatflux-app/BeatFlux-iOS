84ab4faeaf9ebbd6fc9d0a97b1398818b54e8e13
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-07-05 16:01:13 -0400

    fixed bugs

---
+++ BeatFlux/Views/CreateNewPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateNewPlaylistView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 7/5/23.
+//
+
+import SwiftUI
+
+struct CreateNewPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateNewPlaylistView()
+}
