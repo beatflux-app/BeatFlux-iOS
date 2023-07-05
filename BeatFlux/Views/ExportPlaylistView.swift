9973f8082252bc4ebd60ebb5aad4f71b0ec2cbed
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-07-05 07:31:19 -0400

    patched bugs

---
+++ BeatFlux/Views/ExportPlaylistView.swift
@@ -0,0 +1,18 @@
+//
+//  ExportPlaylistView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 7/4/23.
+//
+
+import SwiftUI
+
+struct ExportPlaylistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExportPlaylistView()
+}
