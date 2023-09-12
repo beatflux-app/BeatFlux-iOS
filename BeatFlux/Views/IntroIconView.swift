81948dc619c8bdc9606df33d5032e90eecccdbef
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-09-12 17:29:00 -0400

    fixed bugs and crashing

---
+++ BeatFlux/Views/IntroIconView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroIconView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 9/12/23.
+//
+
+import SwiftUI
+
+struct IntroIconView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroIconView()
+}
