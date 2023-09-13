8fc97fec32b5aeacedd9b50c76cf89f2a0ee95fa
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-09-12 21:30:04 -0400

    implemented version history

---
+++ BeatFlux/Views/PlaylistVersionHistory.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistVersionHistory.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 9/12/23.
+//
+
+import SwiftUI
+
+struct PlaylistVersionHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaylistVersionHistory()
+}
