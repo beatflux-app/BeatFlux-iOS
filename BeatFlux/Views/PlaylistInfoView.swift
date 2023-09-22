cc81b727d9d03e3e16e118c4d54c203360010fba
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-09-21 22:33:52 -0400

    added playlist info view

---
+++ BeatFlux/Views/PlaylistInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  PlaylistInfoView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 9/21/23.
+//
+
+import SwiftUI
+
+struct PlaylistInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlaylistInfoView()
+}
