d6a4a2f785a693c12a498021278f07c801b0ade8
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-09-24 19:00:14 -0400

    stable

---
+++ BeatFlux/Views/SettingsViews/SecuritySettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SecuritySettingsView.swift
+//  BeatFlux
+//
+//  Created by Ari Reitman on 9/24/23.
+//
+
+import SwiftUI
+
+struct SecuritySettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecuritySettingsView()
+}
